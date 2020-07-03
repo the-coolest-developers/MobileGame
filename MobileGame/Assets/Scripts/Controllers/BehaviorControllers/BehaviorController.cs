da52ac0a2d6ff1b9dbf1f31014cc22b4fdbe8fe1
Artiom Ciobanu <aciobanumain@gmail.com>
2020-07-03 23:46:39 +0300

    BehaviorController created

---
+++ MobileGame/Assets/Scripts/Controllers/BehaviorControllers/BehaviorController.cs
@@ -0,0 +1,24 @@
+﻿using Controllers.EntityControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.BehaviorControllers
+{
+    public class BehaviorController : MonoBehaviour
+    {
+        BattleController BattleController;
+        AnimationController AnimationController;
+        MovementController MovementController;
+
+        protected void Start()
+        {
+            BattleController = GetComponent<BattleController>();
+            AnimationController = GetComponent<AnimationController>();
+            MovementController = GetComponent<MovementController>();
+        }
+    }
+}
