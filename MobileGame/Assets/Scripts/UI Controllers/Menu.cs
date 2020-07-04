b784078c1aed73181da4800e7bb70f6dad98206b
Anton179 <anton179971@gmail.com>
2020-07-04 17:25:45 +0300

    Menu (New Game)

--- MobileGame/Assets/Scripts/UI Controllers/Menu.cs
+++ MobileGame/Assets/Scripts/UI Controllers/Menu.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    GameObject NewGame;
+    GameObject MainCamera;
+    GameObject SecondCamera;
+    GameObject SecondCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NewGame = GameObject.FindWithTag("TextN");
+        NewGame.gameObject.SetActive(false);
+        MainCamera = GameObject.Find("Main Camera");
+        SecondCamera = GameObject.Find("SecondCamera");
+        SecondCamera.gameObject.SetActive(false);
+        SecondCanvas = GameObject.Find("SecCanvas");
+        SecondCanvas.gameObject.SetActive(false);
     }
-
+    
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void OpenScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void SetActive(GameObject NewGame)
+    {
+        NewGame.gameObject.SetActive(true);
+    }
+
+    public void SetNotActive(GameObject NewGame)
+    {
+        NewGame.gameObject.SetActive(false);
     }
 }
