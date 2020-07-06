75180da9bddd1ced5eecf75cee2945cb17abc562
Anton179 <anton179971@gmail.com>
2020-07-06 16:46:49 +0300

    Some fixes

--- MobileGame/Assets/Scripts/UI Controllers/GameMenu.cs
+++ MobileGame/Assets/Scripts/UI Controllers/GameMenu.cs
@@ -8,12 +8,14 @@ public class GameMenu : MonoBehaviour
     public GameObject gameMenu;
     public GameObject gameSettings;
     public GameObject ExitMenu;
+    public GameObject SettingsCamera;
     // Start is called before the first frame update
     void Start()
     {
         gameMenu.gameObject.SetActive(false);
         gameSettings.gameObject.SetActive(false);
         ExitMenu.gameObject.SetActive(false);
+        SettingsCamera.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
