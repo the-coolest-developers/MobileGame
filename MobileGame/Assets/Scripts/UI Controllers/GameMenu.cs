f7bdd0417c81ef8416dbef67f393c52408a23a6a
Artiom Ciobanu <aciobanumain@gmail.com>
2020-07-10 18:18:46 +0300

    Несколько изменений
    1. Элементы в сцене сгруппированы и переименованы
    2. Созданы методы для событий клика в классе GameMenu - т.е. пересено из редактора в код
    3. Код стал короче

--- MobileGame/Assets/Scripts/UI Controllers/GameMenu.cs
+++ MobileGame/Assets/Scripts/UI Controllers/GameMenu.cs
@@ -1,23 +1,65 @@
-﻿using System.Collections;
+﻿using Controllers;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
-    GameObject Obj;
+    GameController GameController { get; set; }
 
-    // Start is called before the first frame update
+    GameObject MainCanvasObject { get; set; }
+    GameObject SettingsCanvasObject { get; set; }
+    GameObject GameMenuObject { get; set; }
+    GameObject ExitMenuObject { get; set; }
+
     void Start()
     {
-        Obj = GameObject.Find("GameMenu");
-        Obj.SetActive(false);
-        Obj = GameObject.Find("SettingsCanvas");
-        Obj.SetActive(false);
-        Obj = GameObject.FindWithTag("ExitToMenu");
-        Obj.SetActive(false);
-        Obj = GameObject.Find("SettingsCamera");
-        Obj.SetActive(false);
+        GameController = GameObject.Find("GameControllerObject").GetComponent<GameController>();
+
+        SettingsCanvasObject = GameObject.Find("SettingsCanvas");
+        MainCanvasObject = GameObject.Find("Canvas");
+
+        GameMenuObject = GameObject.Find("GameMenu");
+        ExitMenuObject = GameObject.Find("ConfirmExitMenu");
+
+        GameMenuObject.SetActive(false);
+        SettingsCanvasObject.SetActive(false);
+        ExitMenuObject.SetActive(false);
+        GameObject.Find("SettingsCamera").SetActive(false);
+    }
+
+    public void MenuButton_Click()
+    {
+        GameMenuObject.SetActive(true);
+        GameController.PauseGame();
+    }
+    public void ResumeButton_Click()
+    {
+        GameMenuObject.SetActive(false);
+        GameController.ResumeGame();
+    }
+    public void SettingsButton_Click()
+    {
+        SettingsCanvasObject.SetActive(true);
+        MainCanvasObject.SetActive(false);
+    }
+    public void ExitButton_Click()
+    {
+        ExitMenuObject.SetActive(true);
+    }
+    public void ExitSettingsButton_Click()
+    {
+        SettingsCanvasObject.SetActive(false);
+        MainCanvasObject.SetActive(true);
+    }
+    public void ConfirmExitButton_Click()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void CancelExitButton_Click()
+    {
+        ExitMenuObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,7 +67,6 @@ public class GameMenu : MonoBehaviour
     {
         obj.SetActive(true);
     }
-
     public void SetNotActive(GameObject obj)
     {
         obj.SetActive(false);
