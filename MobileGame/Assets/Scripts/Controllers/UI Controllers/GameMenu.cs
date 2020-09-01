1a8be7edbdf81dd990ff9780da76dc59918c42fa
Artiom Ciobanu <aciobanumain@gmail.com>
2020-09-01 11:35:46 +0300

    Инвентарь можно открыть

--- MobileGame/Assets/Scripts/Controllers/UI Controllers/GameMenu.cs
+++ MobileGame/Assets/Scripts/Controllers/UI Controllers/GameMenu.cs
@@ -7,6 +7,7 @@ namespace Controllers.UI_Controllers
     {
         private GameObject SettingsWindowObject { get; set; }
         private GameObject GameMenuObject { get; set; }
+        private GameObject GameInteractiveMenuObject { get; set; }
         private GameObject ExitDialogObject { get; set; }
 
         private GameObject MenuButtonObject { get; set; }
@@ -17,6 +18,7 @@ namespace Controllers.UI_Controllers
             SettingsWindowObject = GameObject.Find("SettingsWindow");
 
             GameMenuObject = GameObject.Find("GameMenu");
+            GameInteractiveMenuObject = GameObject.Find("GameInteractiveMenu");
             ExitDialogObject = GameObject.Find("ConfirmExitDialog");
 
             MenuButtonObject = GameObject.Find("MenuButton");
@@ -26,6 +28,7 @@ namespace Controllers.UI_Controllers
             GameMenuObject.SetActive(false);
             SettingsWindowObject.SetActive(false);
             ExitDialogObject.SetActive(false);
+            GameInteractiveMenuObject.SetActive(false);
         }
 
         public void MenuButton_Click()
@@ -36,6 +39,7 @@ namespace Controllers.UI_Controllers
             MenuButtonObject.SetActive(false);
             DimmedBackgroundObject.SetActive(true);
         }
+
         public void ResumeButton_Click()
         {
             GameController.ResumeGame();
@@ -44,29 +48,43 @@ namespace Controllers.UI_Controllers
             MenuButtonObject.SetActive(true);
             DimmedBackgroundObject.SetActive(false);
         }
+
         public void SettingsButton_Click()
         {
             SettingsWindowObject.SetActive(true);
             GameMenuObject.SetActive(false);
         }
+
         public void ExitButton_Click()
         {
             ExitDialogObject.SetActive(true);
             GameMenuObject.SetActive(false);
         }
+
         public void ExitSettingsButton_Click()
         {
             SettingsWindowObject.SetActive(false);
             GameMenuObject.SetActive(true);
         }
+
         public void ConfirmExitButton_Click()
         {
             SceneManager.LoadScene("Menu");
         }
+
         public void CancelExitButton_Click()
         {
             ExitDialogObject.SetActive(false);
             GameMenuObject.SetActive(true);
+        }
+
+        public void InteractiveMenuButton_Click()
+        {
+            GameController.PauseGame();
+
+            GameInteractiveMenuObject.SetActive(true);
+            MenuButtonObject.SetActive(false);
+            DimmedBackgroundObject.SetActive(true);
         }
     }
 }
