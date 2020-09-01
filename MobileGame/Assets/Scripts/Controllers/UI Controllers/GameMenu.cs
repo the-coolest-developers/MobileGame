74bfe787735412460b18b0c3a54ae29750bd3199
Artiom Ciobanu <aciobanumain@gmail.com>
2020-09-01 15:00:50 +0300

    Окна скрываются при нажатии на фон

--- MobileGame/Assets/Scripts/Controllers/UI Controllers/GameMenu.cs
+++ MobileGame/Assets/Scripts/Controllers/UI Controllers/GameMenu.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Controllers.UI_Controllers
@@ -7,10 +8,11 @@ namespace Controllers.UI_Controllers
     {
         private GameObject SettingsWindowObject { get; set; }
         private GameObject GameMenuObject { get; set; }
-        private GameObject GameInteractiveMenuObject { get; set; }
+        private GameObject InteractiveMenuObject { get; set; }
         private GameObject ExitDialogObject { get; set; }
 
         private GameObject MenuButtonObject { get; set; }
+        private GameObject InteractiveMenuButtonObject { get; set; }
         private GameObject DimmedBackgroundObject { get; set; }
 
         private void Start()
@@ -18,35 +20,64 @@ namespace Controllers.UI_Controllers
             SettingsWindowObject = GameObject.Find("SettingsWindow");
 
             GameMenuObject = GameObject.Find("GameMenu");
-            GameInteractiveMenuObject = GameObject.Find("GameInteractiveMenu");
+            InteractiveMenuObject = GameObject.Find("InteractiveMenu");
             ExitDialogObject = GameObject.Find("ConfirmExitDialog");
 
+            InteractiveMenuButtonObject = GameObject.Find("InteractiveMenuButton");
             MenuButtonObject = GameObject.Find("MenuButton");
             DimmedBackgroundObject = GameObject.Find("DimmedBackground");
 
+            ResetUiLayout();
+        }
+
+        private void ResetUiLayout()
+        {
             DimmedBackgroundObject.SetActive(false);
+
             GameMenuObject.SetActive(false);
             SettingsWindowObject.SetActive(false);
             ExitDialogObject.SetActive(false);
-            GameInteractiveMenuObject.SetActive(false);
+            InteractiveMenuObject.SetActive(false);
+
+            MenuButtonObject.SetActive(true);
+            InteractiveMenuButtonObject.SetActive(true);
+        }
+
+        private void CloseWindowsAndResume()
+        {
+            ResetUiLayout();
+
+            GameController.ResumeGame();
+        }
+
+        private void PauseAndOpenWindow(GameObject window)
+        {
+            GameController.PauseGame();
+            MenuButtonObject.SetActive(false);
+            InteractiveMenuButtonObject.SetActive(false);
+            DimmedBackgroundObject.SetActive(true);
+
+            window.SetActive(true);
         }
 
         public void MenuButton_Click()
         {
-            GameController.PauseGame();
+            PauseAndOpenWindow(GameMenuObject);
+        }
 
-            GameMenuObject.SetActive(true);
-            MenuButtonObject.SetActive(false);
-            DimmedBackgroundObject.SetActive(true);
+        public void InteractiveMenuButton_Click()
+        {
+            PauseAndOpenWindow(InteractiveMenuObject);
+        }
+
+        public void DimmedBackground_Click()
+        {
+            CloseWindowsAndResume();
         }
 
         public void ResumeButton_Click()
         {
-            GameController.ResumeGame();
-
-            GameMenuObject.SetActive(false);
-            MenuButtonObject.SetActive(true);
-            DimmedBackgroundObject.SetActive(false);
+            CloseWindowsAndResume();
         }
 
         public void SettingsButton_Click()
@@ -76,15 +107,6 @@ namespace Controllers.UI_Controllers
         {
             ExitDialogObject.SetActive(false);
             GameMenuObject.SetActive(true);
-        }
-
-        public void InteractiveMenuButton_Click()
-        {
-            GameController.PauseGame();
-
-            GameInteractiveMenuObject.SetActive(true);
-            MenuButtonObject.SetActive(false);
-            DimmedBackgroundObject.SetActive(true);
         }
     }
 }
